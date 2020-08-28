3ee585c9891fe4a123a357d38e999e526d1f8c76
maxvl2000 <maxvl@mail.ru>
2020-08-28 04:13:28 +0300

    Add project files.

---
+++ image_summer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace image_summer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.of = new System.Windows.Forms.OpenFileDialog();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.total);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(991, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.picture);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(991, 560);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // of
+            // 
+            this.of.FileName = "openFileDialog1";
+            // 
+            // picture
+            // 
+            this.picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picture.Location = new System.Drawing.Point(0, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(991, 560);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            this.picture.Paint += new System.Windows.Forms.PaintEventHandler(this.picture_Paint);
+            this.picture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picture_MouseDown);
+            this.picture.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picture_MouseMove);
+            this.picture.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "total:";
+            // 
+            // total
+            // 
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.total.Location = new System.Drawing.Point(350, 20);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(162, 39);
+            this.total.TabIndex = 2;
+            this.total.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 626);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.OpenFileDialog of;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ image_summer/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_summer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (of.ShowDialog() != DialogResult.OK)
+                return;
+            try
+            {
+                picture.Image = new Bitmap(of.FileName);
+            }
+            catch { return; }
+            }
+        int x0 = 0, y0 = 0, x1=0, y1=0;
+
+        private void picture_Paint(object sender, PaintEventArgs e)
+        {
+            if (picture.Image == null)
+                return;
+
+            Rectangle ee = new Rectangle(x0, y0, x1 - x0, y1 - y0);
+            using (Pen pen = new Pen(Color.Red, 2))
+            {
+                e.Graphics.DrawRectangle(pen, ee);
+            }
+
+            //calc
+            Bitmap b = picture.Image as Bitmap;
+            long t = 0;
+            for (int x = x0; x <= x1 && x < b.Width; x++)
+                for (int y = y0; y <= y1 && y < b.Height; y++)
+                    t += b.GetPixel(x, y).G;
+            total.Text = t.ToString();
+
+        }
+
+        private void picture_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        bool draw = false;
+
+        private void picture_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!draw)
+                return;
+            if (picture.Image == null)
+                return;
+            x1 = e.X;
+            y1 = e.Y;
+
+            picture.Refresh();
+        }
+
+        private void picture_MouseDown(object sender, MouseEventArgs e)
+        {
+            x0 = e.X;
+            y0 = e.Y;
+            draw = true;
+        }
+    }
+}
